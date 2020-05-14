263e7b6a4b61dfe5e283e032c707fd2b73c02eb6
Koen Schipper <koen.schipper@me.com>
2020-05-14 20:51:20 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT =
+  process.env.NODE_ENV === 'production' ? process.env.PORT || 80 : 4000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
